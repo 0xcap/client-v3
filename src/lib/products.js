4fb438bb39272abd5dd6c98d64692ec8551981e0
0xmu <89046951+0xmu@users.noreply.github.com>
2021-12-24 16:07:01 +0400

    price impact update

--- src/lib/products.js
+++ src/lib/products.js
@@ -3,22 +3,22 @@ export const PRODUCTS = {
 		hours: '24/7',
 		logo: '/logos/ETH.svg',
 		baseSpread: 0.00025,
-		maxSlippage: 5,
+		maxSlippage: 8,
 		slippageExponent: 2.5,
 		maxLiquidity: {
-			weth: 2000,
-			usdc: 3800000
+			weth: 200,
+			usdc: 380000
 		}
 	},
 	'BTC-USD': {
 		hours: '24/7',
 		logo: '/logos/BTC.svg',
 		baseSpread: 0.00025,
-		maxSlippage: 5,
+		maxSlippage: 8,
 		slippageExponent: 2.5,
 		maxLiquidity: {
-			weth: 2000,
-			usdc: 3800000
+			weth: 200,
+			usdc: 380000
 		}
 	}
 };
