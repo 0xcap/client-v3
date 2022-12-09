32bf3b2db4d7c964409be52b79bdf340b0c259d1
0xmu <89046951+0xmu@users.noreply.github.com>
2022-12-09 18:33:58 +0000

    Bumping USD vol to account for ETH price decline

--- src/lib/graph.js
+++ src/lib/graph.js
@@ -40,7 +40,7 @@ export async function getVolume() {
   	// console.log('datas', datas);
   	
 	let volumeETH = 420000; // cumulative previous versions of CAP
-	let volumeUSD = 0; // v2 + v1
+	let volumeUSD = 2500000000; // v2 + v1
 	for (const d of datas) {
 		// console.log('d', d);
 		if (d.id == ADDRESS_ZERO) { // ETH
