c1bdc684c26fc567202d8a7059977cae9011b9ba
kappacappa <kappacap@proton.me>
2023-02-18 19:26:09 +0400

    p/l liq fix

--- src/lib/products.js
+++ src/lib/products.js
@@ -2,23 +2,23 @@ export const PRODUCTS = {
 	'ETH-USD': {
 		hours: '24/7',
 		logo: '/logos/ETH.svg',
-		baseSpread: 0.00025,
+		baseSpread: 0.0001,
 		maxSlippage: 5,
 		slippageExponent: 2.5,
 		maxLiquidity: {
-			weth: 800,
-			usdc: 2500000
+			weth: 2000,
+			usdc: 8000000
 		}
 	},
 	'BTC-USD': {
 		hours: '24/7',
 		logo: '/logos/BTC.svg',
-		baseSpread: 0.00025,
+		baseSpread: 0.0001,
 		maxSlippage: 5,
 		slippageExponent: 2.5,
 		maxLiquidity: {
-			weth: 800,
-			usdc: 2500000
+			weth: 3000,
+			usdc: 8000000
 		}
 	}
 };
